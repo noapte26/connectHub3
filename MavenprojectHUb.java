03adba068fec854172b3b7039a42fc6cfc3129b3
noapte26 <pinkypiemuffin@gmail.com>
2024-12-06 23:41:01 +0200

    hello

---
+++ MavenprojectHUb.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.connecthub.mavenprojecthub;
+
+/**
+ *
+ * @author hebai
+ */
+public class MavenprojectHUb {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
