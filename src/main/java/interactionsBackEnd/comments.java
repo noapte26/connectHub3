c1a050ed288d949be05f54e1e9ef5da166f8f8a3
NadaMohamed2422 <nadamuhammaad242@gmail.com>
2024-12-18 17:35:21 +0200

    interactions data base

--- src/main/java/interactionsBackEnd/comments.java
+++ src/main/java/interactionsBackEnd/comments.java
@@ -4,10 +4,33 @@
  */
 package interactionsBackEnd;
 
+import Account.UserAccount;
+
 /**
  *
  * @author CONNECT
  */
 public class comments {
+    private UserAccount account ;
+    private String contentId ;
+    private String commentText ;
+
+    public comments(UserAccount account, String contentId, String commentText) {
+        this.account = account;
+        this.contentId = contentId;
+        this.commentText = commentText;
+    }
+
+    public UserAccount getAccount() {
+        return account;
+    }
+
+    public String getContentId() {
+        return contentId;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
     
 }
